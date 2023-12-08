f5a5414c1f60a063654bb93cb48f1cb3e99ab949
blusque <kokutouaikata@outlook.com>
2023-12-08 22:16:43 +0800

    Initial commit

---
+++ BlusqueCraft/shader/Box.fragment.glsl
@@ -0,0 +1,8 @@
+﻿#version 330 core
+
+layout (location = 0) out vec4 f_Color;
+
+void main()
+{
+    f_Color = vec4(0.2, 0.3, 0.8, 1.0);
+}
---
+++ BlusqueCraft/shader/Box.vertex.glsl
@@ -0,0 +1,10 @@
+﻿#version 330 core
+
+layout (location = 0) in vec4 v_Position;
+
+uniform mat4 MVP;
+
+void main()
+{
+    gl_Position = MVP * v_Position;
+}
